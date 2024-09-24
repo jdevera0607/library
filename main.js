15477d95e05a821218776e9cbf6ee5dd5f4e189d
Jade De Vera <jdevera0607@yahoo.com>
2024-09-24 12:23:50 -0400

    Copied files from the sign-up form and redesigned for library

---
+++ main.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('#form-container');
+const button = document.querySelector('.btn');
+
+const myLibrary = [];
+function Book(){
+
+}
+
+function addBookToLibrary(){
+
+}
+button.addEventListener("click", (e) => {
+    form.classList.toggle("form-container");
+});
