7c7961e95df4c94ae0617f1979265ab5a6ed572c
Jade De Vera <jdevera0607@yahoo.com>
2024-09-25 16:55:28 -0400

    Added display and delete function

--- main.js
+++ main.js
@@ -1,35 +1,70 @@
 const form = document.querySelector('#form-container');
-const button = document.querySelector('.btn');
+const start = document.querySelector('.btn');
 const submit = document.querySelector('.submit');
+const list = document.querySelector('.list-container');
 
+const authorInput = document.querySelector('#author');
+const titleInput = document.querySelector('#title');
+const pagesInput = document.querySelector('#pages');
+const readInput = document.querySelector('#read');
 
-const authorInput = document.querySelector('.author');
-const titleInput = document.querySelector('.title');
-const pagesInput = document.querySelector('.pages');
-const readInput = document.querySelector('.read');
-
+const bookLibrary = [];
 
 function Book(author, title, pages, read){
+
     this.author = author;
     this.title = title;
     this.pages = pages;
     this.read = read;
 
-    const bookLibrary = [];
     bookLibrary.push(author, title, pages, read);
-    alert(bookLibrary);
+    displayBook();
+    clearBook();
+    clearForm();
 }
 
 function addBookToLibrary(){
 
-    const value1 = authorInput.value;
-    const value2 = titleInput.value;
-    const value3 = pagesInput.value;
-    const value4  = readInput.value;
+    let authorVal = authorInput.value;
+    let titleVal = titleInput.value;
+    let pagesVal = pagesInput.value;
+    let readVal  = readInput.value;
 
-    const book = new Book(value1, value2, value3, value4);
+    const book = new Book(authorVal,titleVal,pagesVal,readVal);
 }
-button.addEventListener("click", (e) => {
+
+function displayBook(){
+    const library = document.createElement('ul');
+    const deleteBtn = document.createElement('button');
+    list.appendChild(library);
+    for(let i = 0; i < bookLibrary.length; i++){
+        const listItem = document.createElement('li');
+        library.appendChild(listItem);
+        listItem.textContent = bookLibrary[i];
+    }
+    deleteBtn.textContent = "Delete";
+    library.appendChild(deleteBtn); 
+   
+    deleteBtn.addEventListener('click', () => {
+        library.remove();
+    })
+}
+function clearForm(){
+    authorInput.value = '';
+    titleInput.value = '';
+    pagesInput.value = '';
+    readInput.value = '';
+}
+function clearBook(){
+    while(bookLibrary.length > 0) {
+        bookLibrary.pop();
+    }
+}
+submit.addEventListener('click', () => {
+    addBookToLibrary();
+});
+
+start.addEventListener("click", (e) => {
     form.classList.toggle("hidden");
 });
 
